701fd035e65d495bc5453b7285f0faedd1e278d3
nvshah <nipunshah6776@gmail.com>
2020-07-08 21:54:13 +0530

    starting initial commit ( first )

---
+++ android/app/src/main/kotlin/com/example/awsome_places/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.awsome_places
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
